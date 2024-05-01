0431ca883d00099c2b414be402e07be24de365a3
Ryan-Doolittle <Rdoolittle90@gmail.com>
2024-05-01 16:26:16 -0500

    Revert "Bug Fixes"
    This reverts commit ad459cc3587f2e68d58faa7c8ed8b3e8f4c1cee9.

--- src/containers/control_panel.py
+++ src/containers/control_panel.py
@@ -1,71 +1,22 @@
 import time
 import pandas as pd
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import QThread, pyqtSignal
+
+from PyQt5.QtWidgets import QToolBar
+from PyQt5.QtWidgets import QTableWidget
+from PyQt5.QtWidgets import QTableWidgetItem
+from PyQt5.QtWidgets import QVBoxLayout
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtWidgets import QHBoxLayout
+from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QProgressBar
 
 from ..utilities.import_cleaner import clean_email_list
 from ..utilities.state import state_manager
 from ..utilities.config import config
 
 
-class EmailSenderThread(QThread):
-    update_progress = pyqtSignal(int, str)
-    finished = pyqtSignal()
-    error_occurred = pyqtSignal(str)
-
-    def __init__(self, emails_df, parent_frame):
-        super().__init__()
-        self.emails_df = emails_df
-        self.parent_frame = parent_frame
-        self.keep_running = True
-
-    def run(self):
-        print("Starting email sending process.")
-        recipient_count = len(self.emails_df)
-        for index, (idx, row) in enumerate(self.emails_df.iterrows()):
-            if not self.keep_running:
-                print("Email sending cancelled by user.")
-                break
-
-            if row["Status"] == "Sent":
-                print(f"Skipping already sent email to: {row['Email Address']}")
-                continue
-
-            if not state_manager.can_send_email():
-                self.error_occurred.emit("Daily email limits met")
-                print("Daily email limit reached, stopping send.")
-                break
-
-            recipient = row['Email Address']
-            subject = self.parent_frame.subjectLineEdit.text()
-            raw_content = self.parent_frame.editor.toPlainText()
-            content = self.parent_frame.editor.toHtml()
-            
-            if not subject or not raw_content:
-                self.error_occurred.emit("Subject or content cannot be empty.")
-                return
-
-            try:
-                if state_manager.increment_sent():
-
-                    success = self.parent_frame.gmail_service.send_email(recipient, subject, raw_content, content)
-                    new_status = "Sent" if success else "Failed"
-                    print(f"Email sent to {recipient}: {new_status}")
-            except Exception as e:
-                new_status = "Failed"
-                self.error_occurred.emit(str(e))
-                print(f"Error sending email to {recipient}: {e}")
-
-            self.emails_df.at[idx, 'Status'] = new_status
-            self.update_progress.emit(index, new_status)
-            time.sleep(int(config.get("PREFERENCES","email_delay")))
-
-        print("Email sending process completed.")
-        self.finished.emit()
-
-    def stop(self):
-        self.keep_running = False
-        print("Stopping email sending process...")
 
 
 class ControlPanel(QToolBar):
@@ -76,10 +27,9 @@ class ControlPanel(QToolBar):
         self.emails_df = pd.DataFrame(columns=['Status', 'Email Address'])
         self.setWindowTitle('Recipients')
         self.initUI()
-        self.email_sender_thread = None
-        
-    def displayError(self, message):
-            QMessageBox.critical(self, "Error", message)
+
+        self.sending_emails = False
+
 
     def initUI(self):
         self.widget = QWidget()
@@ -146,23 +96,67 @@ class ControlPanel(QToolBar):
             QMessageBox.critical(self, "Error", "Emailer service is not initialized.")
             return
 
-        self.email_sender_thread = EmailSenderThread(self.emails_df, self.parent_frame)
-        self.email_sender_thread.error_occurred.connect(self.displayError)
-        self.email_sender_thread.update_progress.connect(self.updateEmailStatus)
-        self.email_sender_thread.finished.connect(self.emailSendingFinished)
-        self.email_sender_thread.start()
+        self.sending_emails = True
+        self.progressBar.show()
+        QApplication.processEvents()
 
-    def updateEmailStatus(self, index, status):
-        self.recipientsTable.setItem(index, 0, QTableWidgetItem(status))
-        progress_percentage = int((index + 1) / len(self.emails_df) * 100)
-        self.progressBar.setValue(progress_percentage)
+        subject = self.parent_frame.subjectLineEdit.text()
+        raw_content = self.parent_frame.editor.toPlainText()
+        content = self.parent_frame.editor.toHtml()
 
-    def emailSendingFinished(self):
+        if subject == None or subject == "":
+            QMessageBox.critical(self, "Error", "No Subject")
+            self.sending_emails = False
+            return
+        if raw_content == None or raw_content == "":
+            QMessageBox.critical(self, "Error", "No Content")
+            self.sending_emails = False
+            return
+
+        recipient_count = len(self.emails_df)
+
+        for index, (idx, row) in enumerate(self.emails_df.iterrows()):
+
+            if not state_manager.can_send_email():
+                QMessageBox.critical(self, "Error", "Daily email limits met")
+                return
+            
+            if row["Status"] == "sent":
+                continue
+
+            if not self.sending_emails:
+                self.progressBar.hide()
+                break
+
+            recipient = row['Email Address']
+            try:
+                if state_manager.increment_sent():
+                    success = self.parent_frame.gmail_service.send_email(recipient, subject, raw_content, content)
+                    if success:
+                        new_status = "Sent"
+                    else:
+                        new_status = "Failed"
+
+            except Exception as e:
+                print(e)
+                new_status = "Failed"
+            
+            time.sleep(150)
+
+            self.emails_df.at[idx, 'Status'] = new_status
+            self.recipientsTable.setItem(index, 0, QTableWidgetItem(new_status))
+
+            progress_percentage = int((index + 1) / recipient_count * 100)
+            self.progressBar.setValue(progress_percentage)
+            QApplication.processEvents()
+
         self.progressBar.hide()
+        self.sending_emails = False
+
 
     def cancelSendingEmails(self):
-        if self.email_sender_thread:
-            self.email_sender_thread.stop()
+        self.sending_emails = False
 
+        
     def refreshRecipientsList(self):
         self.loadEmails(self.email_listing)
